58c8c0d8e8ae27d5d2f46a83f41830979da0acf9
rucm <rucm.info@gmail.com>
2018-11-01 23:06:22 +0900

    Changed design of domain.

--- domain/auth.go
+++ domain/auth.go
@@ -2,15 +2,19 @@ package domain
 
 import "net/http"
 
-// Session interface
-type Session interface {
-	Set(cookies []*http.Cookie)
-	Write(object interface{}) error
-	Read(object interface{}) error
+// Session data
+type Session struct {
+}
+
+// SessionDriver is interface
+type SessionDriver interface {
+	New(cookies []*http.Cookie) *Session
+	Write(session *Session, object interface{}) error
+	Read(session *Session, object interface{}) error
 }
 
 // Auth interface
 type Auth interface {
-	Login(username string, password string) error
-	Logout() error
+	Login(username string, password string) *Session
+	Logout(session *Session) error
 }

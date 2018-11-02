f3ab08354fff54b262d975bc8861a1ec30119126
rucm <rucm.info@gmail.com>
2018-11-03 00:07:13 +0900

    Added function of new session.

--- domain/auth/session.go
+++ domain/auth/session.go
@@ -3,19 +3,28 @@ package auth
 import (
 	"encoding/gob"
 	"io"
+	"net/http"
 )
 
 // Session data
-type Session struct {
-	sessions map[string]string
+type Session map[string]string
+
+// NewSession create session from cookies
+func NewSession(cookies []*http.Cookie) *Session {
+	session := Session{}
+	for _, c := range cookies {
+		session[c.Name] = c.Value
+	}
+
+	return &session
 }
 
-func (s *Session) Write(w io.Writer) {
+func (session *Session) Write(w io.Writer) {
 	encoder := gob.NewEncoder(w)
-	encoder.Encode(&s.sessions)
+	encoder.Encode(session)
 }
 
-func (s *Session) Read(r io.Reader) {
+func (session *Session) Read(r io.Reader) {
 	decoder := gob.NewDecoder(r)
-	decoder.Decode(&s.sessions)
+	decoder.Decode(session)
 }

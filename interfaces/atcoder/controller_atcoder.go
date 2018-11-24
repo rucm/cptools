f34cc9507e6bbd92b9cc128886207d571843ac7b
rucm <rucm.info@gmail.com>
2018-11-24 17:32:03 +0900

    Rename some file.

---
+++ interfaces/atcoder/controller_atcoder.go
@@ -0,0 +1,10 @@
+package interfaces
+
+// AtCoderController : login and logout
+type AtCoderController struct {
+}
+
+// Login : login to site
+func (controller *AtCoderController) Login(username string, password string) {
+
+}
---
+++ usecase/auth/interactor.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"github.com/rucm/cptools/domain"
+	"github.com/rucm/cptools/usecase/request"
+)
+
+// AuthInteractor : auth input port
+type AuthInteractor struct {
+	Handler usecase.RequestHandler
+	Repo    usecase.SessionRepository
+}
+
+// Login : login to site
+func (interactor *AuthInteractor) Login(url string, param usecase.Parameter) {
+
+	res := interactor.Handler.Execute(url, param)
+
+	session := &domain.Session{}
+	res.Bind(session)
+
+	interactor.Repo.Write(session)
+}
+
+// Logout : logout to site
+func (interactor *AuthInteractor) Logout() {
+
+	interactor.Repo.Remove()
+}
+
+// Session : if state in login, return session
+func (interactor *AuthInteractor) Session() *domain.Session {
+
+	return interactor.Repo.Read()
+}

0194108abb4a57f59ea8e5bb290e4f062c44ea99
rucm <rucm.info@gmail.com>
2018-11-26 22:21:31 +0900

    Fine tuning method.

--- usecase/request/request.go
+++ usecase/request/request.go
@@ -2,7 +2,7 @@ package usecase
 
 // RequestHandler : handler
 type RequestHandler interface {
-	Execute(url string, param Parameter) Response
+	Execute() Response
 }
 
 // Parameter : request param (post request)

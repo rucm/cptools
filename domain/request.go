7d370c0ba816ef22bcdc31e084757d2305770099
rucm <rucm.info@gmail.com>
2018-12-06 22:59:00 +0900

    Added request in domain.

---
+++ domain/request.go
@@ -0,0 +1,18 @@
+package domain
+
+// RequestHandler : handler
+type RequestHandler interface {
+	Execute() Response
+}
+
+// Parameter : request param (post request)
+type Parameter interface {
+	Get(key string) string
+	Set(key string, value string)
+	Del(key string) error
+}
+
+// Response : response
+type Response interface {
+	Bind(session *Session)
+}

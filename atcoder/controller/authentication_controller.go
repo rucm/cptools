fb2e0208ade5f4c290d074dc39905bfccd82a869
rucm <rucm.info@gmail.com>
2018-12-11 22:01:50 +0900

    Added controller.

---
+++ atcoder/controller/authentication_controller.go
@@ -0,0 +1,16 @@
+package atcoder
+
+import (
+	"github.com/rucm/cptools/atcoder/interactor"
+)
+
+// AuthenticationController :
+type AuthenticationController struct {
+	Interactor atcoder.AuthenticationInteractor
+}
+
+// NewAuthenticationController :
+func NewAuthenticationController() *AuthenticationController {
+
+	return nil
+}

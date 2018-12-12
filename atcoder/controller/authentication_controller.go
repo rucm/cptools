9c87fe9c12f102c490177d41de5a2dfd4b1ec40a
rucm <rucm.info@gmail.com>
2018-12-12 21:39:25 +0900

    Added auth controller.

--- atcoder/controller/authentication_controller.go
+++ atcoder/controller/authentication_controller.go
@@ -1,16 +1,45 @@
 package atcoder
 
 import (
-	"github.com/rucm/cptools/atcoder/interactor"
+	infrastracture "github.com/rucm/cptools/atcoder/infrastracture"
+	interactor "github.com/rucm/cptools/atcoder/interactor"
+	repository "github.com/rucm/cptools/atcoder/repository"
+	"github.com/rucm/cptools/usecase"
 )
 
 // AuthenticationController :
 type AuthenticationController struct {
-	Interactor atcoder.AuthenticationInteractor
+	Interactor usecase.Authentication
 }
 
 // NewAuthenticationController :
 func NewAuthenticationController() *AuthenticationController {
 
-	return nil
+	return &AuthenticationController{
+		Interactor: &interactor.AuthenticationInteractor{
+			Handler: &infrastracture.RequestHandler{
+				URL:         "https://practice.contest.atcoder.jp/login",
+				Method:      "POST",
+				ContentType: "application/x-www-form-urlencoded",
+			},
+			Repo: &repository.SessionFileRepository{
+				Filaname: "atcoder.session",
+			},
+		},
+	}
+}
+
+// Login :
+func (controller *AuthenticationController) Login(user string, password string) {
+
+	param := &infrastracture.Parammeter{}
+	param.Set("name", user)
+	param.Set("password", password)
+	controller.Interactor.Login(param)
+}
+
+// Logout :
+func (controller *AuthenticationController) Logout() {
+
+	controller.Interactor.Logout()
 }

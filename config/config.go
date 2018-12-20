1c4aced69c0d1b1a1b7f313ad31e970c30086c17
rucm <rucm.info@gmail.com>
2018-12-20 23:16:55 +0900

    Added read configuration file.

--- config/config.go
+++ config/config.go
@@ -1,9 +1,42 @@
 package config
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
 // Data :
 type Data struct {
 	Common  Common  `yaml:"common"`
 	AtCoder AtCoder `yaml:"atcoder"`
+}
+
+// Config :
+var Config = defaultData
+
+func init() {
+
+	fileExists := isFileExists(configFile)
+
+	if fileExists {
+		buf, err := ioutil.ReadFile(configFile)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = yaml.Unmarshal(buf, &Config)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		yaml.Marshal(&defaultData)
+	}
 }
 
 const configFile = "cptools_config.yaml"
@@ -18,6 +51,8 @@ var defaultData = Data{
 	},
 }
 
-func init() {
+func isFileExists(filename string) bool {
 
+	_, err := os.Stat(filename)
+	return err == nil
 }
